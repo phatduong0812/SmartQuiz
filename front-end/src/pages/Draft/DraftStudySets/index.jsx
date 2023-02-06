210322b6b324ca0b7a0723bc154b863d5c7a3ead
Hiển <hienbui25g@gmail.com>
2023-02-07 01:19:58 +0700

    implement create draft

--- front-end/src/pages/Draft/DraftStudySets/index.jsx
+++ front-end/src/pages/Draft/DraftStudySets/index.jsx
@@ -5,8 +5,8 @@ import DraftCard from './DraftCard'
 const DraftList = ({ studysets }) => {
     return (
         <React.Fragment>
-            {studysets.map((studyset, index) => (
-                <DraftCard key={studyset.id} index={index} studyset={studyset} />
+            {studysets.studySet.map((studyset) => (
+                <DraftCard key={studyset.id} studyset={studyset} path={studysets.path} />
             ))}
         </React.Fragment>
     )
--- front-end/src/pages/Draft/index.jsx
+++ front-end/src/pages/Draft/index.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-
 import { Box, Typography } from '@mui/material'
 
 import DraftList from './DraftStudySets'
 
-import { Mock_Data } from '~/Mock'
+import LocalStorageUtils from '~/utils/LocalStorageUtils'
 
 const Draft = () => {
+    const draftQuestions = LocalStorageUtils.getItem('drafts')
     return (
         <Box maxWidth={1390} container spacing={4} sx={{ m: '0 auto', mt: 2, mb: 5 }}>
             <Typography mb={5} variant="h4" sx={{ fontWeight: 'bold' }}>
                 Your Draft
             </Typography>
-            <DraftList studysets={Mock_Data.recent} />
+            <DraftList studysets={draftQuestions} />
         </Box>
     )
 }
