287b82223ce86dddc50a07a70a471bdc47416b17
phatduong <phatduong0812@gmail.com>
2023-02-13 08:04:05 +0700

    fix login

--- SmartQuizApi/SmartQuizApi/Program.cs
+++ SmartQuizApi/SmartQuizApi/Program.cs
@@ -84,6 +84,8 @@ if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseHttpsRedirection();
+
 app.UseCookiePolicy(new CookiePolicyOptions
 {
     Secure = CookieSecurePolicy.Always
